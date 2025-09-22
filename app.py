b06bcca8134fa0a8e3c27fa252e0965551a1994c
Nils Steinmetz <nils_steinmetz@web.de>
2025-09-22 15:59:56 +0200

    implemented add functionality and fixed index

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import json
 
 app = Flask(__name__)
 
-
+def read_posts():
+    with open('data.json', 'r')as f:
+        return json.load(f)
 @app.route('/')
 def index():
     with open('data.json', 'r')as f:
-        blog_posts = json.load(f)
+        blog_posts = read_posts()
     return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        posts = read_posts()
+        id = len(posts) + 1
+        posts.append({'id': id, 'author': author, 'title': title, 'content': content})
+        with open('data.json', 'w')as f:
+            json.dump(posts, f)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
